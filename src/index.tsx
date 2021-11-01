cd25ad3ba301952af91bfb83fea8e5666d87c944
Ilovealtapow <Jfloyd421@gmail.com>
2021-11-01 17:02:27 -0600

    day-1

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Home, Dashboard, SignIn } from './components';
+import './styles.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Switch>
+
+        <Route exact path='/'>
+          <Home title={'Car Inventory'}/>
+        </Route>
+
+        <Route path='/dashboard'>
+          <Dashboard></Dashboard>
+        </Route>
+
+        <Route path='/signin'>
+          <SignIn></SignIn>
+        </Route>
+
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
